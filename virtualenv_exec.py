525c108d713a9c33a7e1684d57f8fea125a71cec
Jeremy Thurgood <firxen@gmail.com>
2012-10-25 13:06:50 +0200

    Reformat and cleanup.

--- virtualenv_exec.py
+++ virtualenv_exec.py
@@ -1,20 +1,24 @@
 import os
 
-import sublime, sublime_plugin
+import sublime_plugin
+
 
 class VirtualenvExecCommand(sublime_plugin.WindowCommand):
     def run(self, working_dir="", path=None, virtualenv_dir=None, **kwargs):
-    	view_settings = self.window.active_view().settings()
-    	working_dir = view_settings.get("build_working_dir", working_dir)
+        view_settings = self.window.active_view().settings()
+        working_dir = view_settings.get("build_working_dir", working_dir)
 
-    	if virtualenv_dir is None:
-    		virtualenv_dir = view_settings.get("virtualenv_dir")
-    	print repr(working_dir), repr(virtualenv_dir)
-    	venv_bin_path = os.path.join(virtualenv_dir, "bin")
+        if virtualenv_dir is None:
+            virtualenv_dir = view_settings.get("virtualenv_dir")
 
-    	if path is not None:
-    		path = os.pathsep.join([path, venv_bin_path])
-    	else:
-    		path = venv_bin_path
+        venv_bin_path = os.path.join(virtualenv_dir, "bin")
 
-    	return self.window.run_command("exec", dict(working_dir=working_dir, path=path, **kwargs))
+        if path is not None:
+            path = os.pathsep.join([path, venv_bin_path])
+        else:
+            path = venv_bin_path
+
+        kwargs["working_dir"] = working_dir
+        kwargs["path"] = path
+
+        return self.window.run_command("exec", **kwargs)
